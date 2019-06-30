1f0512bf0f3cef69e009ced61dbd1fb37da9647b
Barbara Johnson <johnson.barbara@gmail.com>
2019-06-30 20:10:33 +0000

    Automatically backed up by Learn

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -30,5 +30,6 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
     @article.update(title: params[:article][:title], description: params[:article][:description])
     redirect_to article_path(@article)
+  end 
     
 end
